5a5bafb0a2ce29a822e46bf28ed0ec18ecb74d59
Matthew DesEnfants <matthew.desenfants@avanade.com>
2019-01-08 11:31:08 -0800

    resolving dependency issues in mstest

---
+++ AtomicCounter.Test.Next/Initialize.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using AtomicCounter.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Storage;
+
+namespace AtomicCounter.Test
+{
+    [TestClass]
+    public class Initialize
+    {
+        public const string Tenant = "test_tenant";
+        public const string App = "test_app";
+        public const string Counter = "test_count";
+
+        public static CloudStorageAccount Storage;
+
+        [AssemblyInitialize]
+        public static async Task AssemblyInitialize(TestContext context)
+        {
+            Environment.SetEnvironmentVariable("AzureWebJobsStorage", "UseDevelopmentStorage=true;");
+            Storage = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+
+            var queueClient = Storage.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference($"{CounterStorage.Sanitize(Tenant)}-{CounterStorage.Sanitize(App)}-{CounterStorage.Sanitize(Counter)}");
+            await queue.DeleteIfExistsAsync();
+            var increments = queueClient.GetQueueReference("increment-items");
+            await queue.DeleteIfExistsAsync();
+
+            var tableClient = Storage.CreateCloudTableClient();
+            var table = tableClient.GetTableReference(CounterStorage.Tableize(Tenant+"counts"));
+            await table.DeleteIfExistsAsync();
+
+            var blobClient = Storage.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("tenants");
+            await blobClient.GetContainerReference("tenants").GetBlockBlobReference(Tenant).DeleteIfExistsAsync();
+        }
+    }
+}
