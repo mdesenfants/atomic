a69d2473444477647be3422c96de3806d44c5b03
Matthew DesEnfants <matt.desenfants@gmail.com>
2019-01-31 23:16:57 -0800

    adding invoice support to counter model

--- AtomicCounter/Models/Counter.cs
+++ AtomicCounter/Models/Counter.cs
@@ -4,18 +4,27 @@ using System.Collections.Generic;
 
 namespace AtomicCounter.Models
 {
+    public enum InvoiceFrequency
+    {
+        Never,
+        Weekly,
+        TwiceMonthly,
+        Monthly,
+        Quarterly,
+        TwiceAnnually,
+        Annually
+    }
+
     public class Counter
     {
         public string CounterName { get; set; }
-
         public HashSet<Guid> Profiles { get; set; } = new HashSet<Guid>();
-
-        public IList<string> Origins { get; set; } = new List<string>();
-
         public IList<string> WriteKeys { get; set; } = new List<string>();
-
         public IList<string> ReadKeys { get; set; } = new List<string>();
-
         public IList<PriceChangeEvent> PriceChanges { get; set; } = new List<PriceChangeEvent>();
+        public DateTimeOffset LastInvoiceRun { get; set; } = DateTimeOffset.MinValue;
+        public DateTimeOffset NextInvoiceRun { get; set; } = DateTimeOffset.MaxValue;
+        public InvoiceFrequency InvoiceFrequency { get; set; } = InvoiceFrequency.Never;
+        public bool AutoSubmit { get; set; } = false;
     }
 }
